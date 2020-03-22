711cc8b7c5111573aaa85ea02a8a7e1225456664
L. K <LK00100100@gmail.com>
2020-03-22 11:56:07 -0400

    added table

--- js/simulatePi.js
+++ js/simulatePi.js
@@ -1,7 +1,9 @@
-// Terminology i use
-// canvas: the entire drawing space
-// inner square: takes up sub-space within the canvas. These are blue dots
-// inner circle: within the inner square. These are orange dots.
+/**
+ * Terminology i use
+ * canvas: the entire drawing space
+ * inner square: takes up sub-space within the canvas. These are blue dots
+ * inner circle: within the inner square. These are orange dots.
+ */
 
 //constants used by the #circle-canvas
 const CANVAS_CONST = {
@@ -29,27 +31,43 @@ function initCanvas() {
     canvas.setAttribute("width", CANVAS_CONST.MAX_WIDTH);
 };
 
-//actually does the monte carlo simulation
+//
+/**
+ * actually does the monte carlo simulation 
+ * 
+ * points
+ */
 function simulate() {
     clearCanvas();
-    
+
     //note: you can get this from an input box later...
     let numPointsToGenerate = 1000;
 
     let canvas = document.getElementById("circle-canvas");
     let ctx = canvas.getContext("2d");
 
+    //list of [[x, y, isWithinCircle]...]
+    //ex: [[1,2 , true]]
+    let points = [];
+
     //get a bunch of random points and draw them.
     for (let i = 0; i < numPointsToGenerate; i++) {
         let random_x = getRandomIntInclusive(INNER_SQUARE.SQUARE_LEFT_X, INNER_SQUARE.SQUARE_RIGHT_X);
         let random_y = getRandomIntInclusive(INNER_SQUARE.SQUARE_UP_Y, INNER_SQUARE.SQUARE_DOWN_Y);
 
+        let isWithinCircle = isWithinInnerCircle(random_x, random_y);
+
         let targetColor = "blue";
-        if(isWithinInnerCircle(random_x, random_y))
+        if (isWithinCircle)
             targetColor = "orange";
 
         drawCircle(ctx, random_x, random_y, targetColor);
+
+        points.push([random_x, random_y, isWithinCircle]);
     }
+
+    //update points-table
+    updatePointsTable(points);
 };
 
 //clears the canvas completely
@@ -70,7 +88,7 @@ function isWithinInnerCircle(x, y) {
 
     //inner cirlce radius. also inner square length
     let radius = (INNER_SQUARE.SQUARE_RIGHT_X - INNER_SQUARE.SQUARE_LEFT_X) / 2;
-    let circleCenterX =  INNER_SQUARE.SQUARE_LEFT_X + radius;
+    let circleCenterX = INNER_SQUARE.SQUARE_LEFT_X + radius;
     let circleCenterY = INNER_SQUARE.SQUARE_UP_Y + radius;
 
     let fromCenterX = Math.abs(x - circleCenterX);
@@ -117,3 +135,66 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 };
+
+/**
+ * Clears and updates the points table
+ * @param {Array} points an array of arrays. [[x:Number, y:Number, isWithinCircle:Boolean]]
+ */
+function updatePointsTable(points) {
+    let tableElement = document.getElementById("points-table");
+    tableElement.textContent = "";
+
+    //make header
+    let tableRow = document.createElement("tr");
+
+    let headerNum = document.createElement("th")
+    headerNum.textContent = "#";
+
+    let headerX = document.createElement("th")
+    headerX.textContent = "X";
+
+    let headerY = document.createElement("th")
+    headerY.textContent = "Y";
+
+    let headerInCircle = document.createElement("th")
+    headerInCircle.textContent = "In Inner Circle";
+
+    tableRow.appendChild(headerNum);
+    tableRow.appendChild(headerX);
+    tableRow.appendChild(headerY);
+    tableRow.appendChild(headerInCircle);
+    tableElement.appendChild(tableRow);
+
+    //create a data row for each point
+    points.forEach((point, rowNum) => {
+        let x = point[0];
+        let y = point[1];
+        let isWithinCircle = point[2];
+
+        let textColor = isWithinCircle ? "orange-text" : "blue-text";
+
+        //create a row
+        let tableRow = document.createElement("tr");
+
+        let dataRowNum = document.createElement("td")
+        dataRowNum.textContent = rowNum;
+
+        let dataX = document.createElement("td")
+        dataX.textContent = x;
+
+        let dataY = document.createElement("td")
+        dataY.textContent = y;
+
+        let dataInCircle = document.createElement("td")
+        dataInCircle.setAttribute("class", textColor);
+        dataInCircle.textContent = isWithinCircle;
+
+        //add to row and table
+        tableRow.appendChild(dataRowNum);
+        tableRow.appendChild(dataX);
+        tableRow.appendChild(dataY);
+        tableRow.appendChild(dataInCircle);
+        tableElement.appendChild(tableRow);
+    });
+
+}
